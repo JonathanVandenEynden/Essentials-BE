f5e046eccd530efad0c2eb6da2f594ab8fcdf7a4
Sébastien De Pauw <sebastien.depauw@hogent.be>
2020-10-06 13:08:30 +0200

    IUser implenteren

--- P3Backend/P3Backend/Model/ChangeManager.cs
+++ P3Backend/P3Backend/Model/ChangeManager.cs
@@ -5,7 +5,13 @@ namespace P3Backend.Model {
 		public int Id { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
+		public Country Country { get; set; }
+		public Office Office { get; set; }
+		public Factory Factory { get; set; }
+		public Department Department { get; set; }
+		public Team Team { get; set; }
 		public List<ChangeInitiative> changeInitiatives { get; set; }
+		public List<OrganizationPart> OrganizationPart { get; set; }
 	}
 
 }
