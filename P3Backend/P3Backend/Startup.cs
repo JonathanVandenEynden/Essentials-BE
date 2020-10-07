499e0b31a8f9a1bba4d0127bb295c9a3910b63f9
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-07 20:55:39 +0200

    put connectionstring in user secrects

--- P3Backend/P3Backend/Startup.cs
+++ P3Backend/P3Backend/Startup.cs
@@ -25,7 +25,7 @@ namespace P3Backend {
 		public void ConfigureServices(IServiceCollection services) {
 			services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-			
+
 			services.AddScoped<DataInitializer>();
 			services.AddControllers();
 		}
