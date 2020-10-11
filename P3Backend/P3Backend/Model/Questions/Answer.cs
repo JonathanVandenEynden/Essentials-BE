a339fe5dd30fb5b5fee97b3575d35ade3f4976d4
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-11 17:57:10 +0200

    Add possibility to choose multiple answers, Add way to store amountChosen for Answer

---
+++ P3Backend/P3Backend/Model/Questions/Answer.cs
@@ -0,0 +1,15 @@
+﻿namespace P3Backend.Model.Questions {
+	public class Answer {
+		public int Id { get; set; }
+		public string AnswerString { get; set; }
+		public int AmountChosen { get; set; }
+
+		public Answer(string answerString) {
+			AnswerString = answerString;
+
+			AmountChosen = 0;
+		}
+
+		public void ChooseAnswer() => AmountChosen++;
+	}
+}
--- P3Backend/P3Backend/Model/Questions/ClosedQuestion.cs
+++ P3Backend/P3Backend/Model/Questions/ClosedQuestion.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace P3Backend.Model.Questions {
 	public abstract class ClosedQuestion : IQuestion {
-		public IList<string> Awnsers { get; set; }
 
-		public ClosedQuestion(string questionString) : base(questionString) {
-			Awnsers = new List<string>();
+		public IList<Answer> PossibleAnswers { get; set; }
+		public bool IsMulti { get; set; }
+
+		public ClosedQuestion(string questionString, bool multi) : base(questionString) {
+			PossibleAnswers = new List<Answer>();
+			IsMulti = multi;
 		}
 	}
 }
