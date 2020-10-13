52e969938c5d04e490cbeb6adf061e3fa9eb8c24
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-13 19:39:29 +0200

    Add Survey mapping

--- P3Backend/P3Backend/Data/ApplicationDbContext.cs
+++ P3Backend/P3Backend/Data/ApplicationDbContext.cs
@@ -19,18 +19,26 @@ namespace P3Backend.Data {
 		protected override void OnModelCreating(ModelBuilder modelBuilder) {
 			base.OnModelCreating(modelBuilder);
 
+			// Users
 			modelBuilder.Entity<Admin>();
 			modelBuilder.Entity<ChangeManager>();
 			modelBuilder.Entity<Employee>();
+
+			// Assesments
 			modelBuilder.Entity<Survey>();
+			modelBuilder.Entity<OpenQuestion>();
+			modelBuilder.Entity<ClosedQuestion>();
+
+			// ChangeTypes
 			modelBuilder.Entity<TechnologicalChangeType>();
 			modelBuilder.Entity<EconomicalChangeType>();
 			modelBuilder.Entity<OrganizationalChangeType>();
 			modelBuilder.Entity<PersonalChangeType>();
-			modelBuilder.Entity<OpenQuestion>();
-			modelBuilder.Entity<ClosedQuestion>();
-
 			modelBuilder.Entity<ChangeInitiative>();
+
+
+			// configurations
+			modelBuilder.ApplyConfiguration(new SurveyConfiguration());
 			modelBuilder.ApplyConfiguration(new ChangeInitiativeConfiguration());
 
 
---
+++ P3Backend/P3Backend/Data/Mapping/SurveyConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P3Backend.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P3Backend.Data.Mapping {
+	public class SurveyConfiguration : IEntityTypeConfiguration<Survey> {
+		public void Configure(EntityTypeBuilder<Survey> builder) {
+			builder.HasKey(s => s.Id);
+
+			builder.Property(s => s.AmountSubmitted);
+
+			builder.HasMany(s => s.Questions).WithOne();
+			builder.HasOne(s => s.Feedback).WithOne();
+
+		}
+	}
+}
