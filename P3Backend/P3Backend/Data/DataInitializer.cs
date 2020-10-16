11325304e9fc536635a0897460e9763b68a09a93
JonathanVandenEynden <47554911+JonathanVandenEynden@users.noreply.github.com>
2020-10-16 14:13:26 +0200

    SurveyController added and implemented +data inserted in DataInitializer + A few DTO's added

--- P3Backend/P3Backend/Data/DataInitializer.cs
+++ P3Backend/P3Backend/Data/DataInitializer.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using P3Backend.Model;
 
 namespace P3Backend.Data {
 	public class DataInitializer {
@@ -14,11 +16,26 @@ namespace P3Backend.Data {
 		}
 
 		public async Task InitializeData() {
-			_dbContext.Database.EnsureDeleted();
+			/*_dbContext.Database.EnsureDeleted();
 			if (_dbContext.Database.EnsureCreated()) {
-				_dbContext.SaveChanges();
+
+                #region Surveys
+                Survey survey1 = new Survey();
+				Survey survey2 = new Survey();				
+				#endregion
+
+				#region RoadmapItems
+				RoadMapItem roadMapItem1 = new RoadMapItem("RoadmapItem number 1", DateTime.Now.AddDays(1), DateTime.Now.AddDays(1).AddHours(1));
+				RoadMapItem roadMapItem2 = new RoadMapItem("RoadmapItem number 2", DateTime.Now.AddDays(2), DateTime.Now.AddDays(2).AddHours(1));
+				roadMapItem1.Assesment = survey1;
+				roadMapItem2.Assesment = survey2;
+				var roadmapItems = new List<RoadMapItem> { roadMapItem1, roadMapItem2 };
+				_dbContext.AddRange(roadmapItems);				
+                #endregion
+
+                _dbContext.SaveChanges();
 				Console.WriteLine("Database created");
-			}
+			}*/
 		}
 	}
 }
