c4d6baf362092ccc057fa7af70302db5c17d9b7a
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-17 15:49:41 +0200

    Add base of OrganizationsController

---
+++ P3Backend/P3Backend/Controllers/OrganizationsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P3Backend.Model;
+using P3Backend.Model.DTO_s;
+using P3Backend.Model.RepoInterfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace P3Backend.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	[Produces("application/json")]
+	public class OrganizationsController : ControllerBase {
+
+		private readonly IOrganizationRepository _organizationRepository;
+
+		public OrganizationsController(IOrganizationRepository organizationRepo) {
+			_organizationRepository = organizationRepo;
+		}
+
+		[HttpGet("{organizationId}")]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public ActionResult<Organization> GetOrganizationById(int organizationId) {
+			Organization o = _organizationRepository.GetBy(organizationId);
+
+			if (o == null) {
+				return NotFound();
+			}
+
+			return o;
+		}
+
+		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public IActionResult PostOrganization(OrganizationDTO dto) {
+			try {
+				Organization newO = new Organization(dto.Name);
+
+				_organizationRepository.Add(newO);
+
+				_organizationRepository.SaveChanges();
+
+				return CreatedAtAction(nameof(GetOrganizationById), new { organizationId = newO.Id }, newO);
+			}
+			catch {
+				return BadRequest();
+			}
+		}
+
+
+		[HttpDelete("{organizationId}")]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		public IActionResult Delete(int organizationId) {
+			Organization oldO = _organizationRepository.GetBy(organizationId);
+
+			if (oldO == null) {
+				return NotFound();
+			}
+
+			_organizationRepository.Delete(oldO);
+			_organizationRepository.SaveChanges();
+
+			return NoContent();
+
+		}
+	}
+}
