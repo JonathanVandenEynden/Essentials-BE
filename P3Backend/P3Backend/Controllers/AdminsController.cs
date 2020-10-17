533de9a05b0fd064f6af0466cb443699c420b34c
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-17 16:36:41 +0200

    add Adminrepo PART1

---
+++ P3Backend/P3Backend/Controllers/AdminsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P3Backend.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	[Produces("application/json")]
+	public class AdminsController : ControllerBase {
+
+		private readonly IAdminRepos
+	}
+}
