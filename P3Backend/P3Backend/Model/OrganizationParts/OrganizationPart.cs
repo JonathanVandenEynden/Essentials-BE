bddf2433823e7a354dee474b99331099abcffd73
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-10-23 18:48:23 +0200

    add users to organizationalpart

--- P3Backend/P3Backend/Model/OrganizationParts/OrganizationPart.cs
+++ P3Backend/P3Backend/Model/OrganizationParts/OrganizationPart.cs
@@ -1,20 +1,21 @@
-﻿namespace P3Backend.Model.OrganizationParts {
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace P3Backend.Model.OrganizationParts {
 	public class OrganizationPart {
-		public int Id {
-			get; set;
-		}
+		public int Id { get; set; }
 
-		public string Name {
-			get; set;
-		}
+		public string Name { get; set; }
 
-		public OrganizationPartType Type {
-			get; set;
-		}
+		public IList<IUser> Users { get; set; }
+
+		public OrganizationPartType Type { get; set; }
 
 		public OrganizationPart(string name, OrganizationPartType type) {
 			Name = name;
 			Type = type;
+
+			Users = new List<IUser>();
 		}
 		protected OrganizationPart() {
 			// EF
