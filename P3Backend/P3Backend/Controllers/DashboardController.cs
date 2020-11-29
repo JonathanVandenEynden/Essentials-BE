b973de468100f403ad20bdbf5a74e30c69a5b85a
Simon De Wilde <simon.dewilde2000@gmail.com>
2020-11-29 10:30:54 +0100

    fix dashboard nullpointer

--- P3Backend/P3Backend/Controllers/DashboardController.cs
+++ P3Backend/P3Backend/Controllers/DashboardController.cs
@@ -10,20 +10,17 @@ using P3Backend.Model.Questions;
 using P3Backend.Model.RepoInterfaces;
 using P3Backend.Model.Users;
 
-namespace P3Backend.Controllers
-{
+namespace P3Backend.Controllers {
 	[Route("api/[controller]")]
 	[ApiController]
 	[Produces("application/json")]
-	public class DashboardController : ControllerBase
-	{
+	public class DashboardController : ControllerBase {
 		private readonly IRoadmapItemRepository _roadmapItemRepository;
 		private readonly IChangeInitiativeRepository _changeInitiativeRepository;
 		private readonly ISurveyRepository _surveyRepository;
 		private readonly IChangeManagerRepository _changeManagerRepository;
 
-		public DashboardController(IRoadmapItemRepository roadMapItemsRepository, IChangeInitiativeRepository changeInitiativeRepository, ISurveyRepository surveyRepository, IChangeManagerRepository changeManagerRepository)
-		{
+		public DashboardController(IRoadmapItemRepository roadMapItemsRepository, IChangeInitiativeRepository changeInitiativeRepository, ISurveyRepository surveyRepository, IChangeManagerRepository changeManagerRepository) {
 			_roadmapItemRepository = roadMapItemsRepository;
 			_changeInitiativeRepository = changeInitiativeRepository;
 			_surveyRepository = surveyRepository;
@@ -39,41 +36,34 @@ namespace P3Backend.Controllers
 		[HttpGet("[action]/{id}")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
-		public ActionResult<double> GetFilledInSurveysOfChangeInitiative(int id)
-		{
+		public ActionResult<double> GetFilledInSurveysOfChangeInitiative(int id) {
 			ChangeInitiative ci = _changeInitiativeRepository.GetBy(id);
 
-			if (ci == null)
-			{
+			if (ci == null) {
 				return NotFound("Item not found");
 			}
 
 			List<IAssessment> surveys = new List<IAssessment>();
-			foreach(var r in ci.RoadMap)
-            {
+			foreach (var r in ci.RoadMap) {
 				surveys.Add(r.Assessment);
-            }
+			}
 
 			var total = surveys.Count();
 			double filledIn = 0;
 
 			List<Question> questions = new List<Question>();
 
-			foreach(var s in surveys)
-            {
-				if (s != null)
-				{
+			foreach (var s in surveys) {
+				if (s != null) {
 					questions.AddRange(s.Questions);
 				}
-            }
+			}
 
-			foreach(var q in questions)
-            {
-				if (q.QuestionRegistered.Count() != 0)
-                {
+			foreach (var q in questions) {
+				if (q.QuestionRegistered.Count() != 0) {
 					filledIn++;
-                }	 
-            }
+				}
+			}
 
 			return ((filledIn / total) * 100);
 		}
@@ -88,8 +78,7 @@ namespace P3Backend.Controllers
 		[HttpGet("[action]/{id}")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
-		public ActionResult<Dictionary<int, int>> GetMoodFromChangeInitiative(int id)
-		{
+		public ActionResult<Dictionary<int, int>> GetMoodFromChangeInitiative(int id) {
 			ChangeInitiative ci = _changeInitiativeRepository.GetBy(id);
 
 			Dictionary<int, int> moods = new Dictionary<int, int>();
@@ -99,38 +88,34 @@ namespace P3Backend.Controllers
 			moods.Add(4, 0);
 			moods.Add(5, 0);
 
-			if (ci == null)
-			{
+			if (ci == null) {
 				return NotFound("Item not found");
 			}
 
 			List<IAssessment> surveys = new List<IAssessment>();
-			foreach (var r in ci.RoadMap)
-			{
+			foreach (var r in ci.RoadMap) {
 				surveys.Add(r.Assessment);
 			}
 
 			List<RangedQuestion> feedback = new List<RangedQuestion>();
 
-			foreach (var s in surveys)
-			{
-				if (s != null)
-                {
+			foreach (var s in surveys) {
+				if (s != null) {
 					feedback.Add(s.Feedback);
-                }
-					
+				}
+
 			}
 
-			foreach(var f in feedback)
-            {
-				foreach (var k in moods.Keys)
-				{
-					if (f != null)
-                    {
-						moods[k] = f.PossibleAnswers[k];
+			foreach (var f in feedback) {
+				if (f != null) {
+					foreach (var k in f.PossibleAnswers.Keys) {
+
+						if (k % 1 == 0 && k != 0) {
+							moods[Convert.ToInt32(k)] += f.PossibleAnswers[k];
+						}
 					}
 				}
-            }
+			}
 
 			return moods;
 		}
