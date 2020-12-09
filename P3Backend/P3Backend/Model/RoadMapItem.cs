0d1c6e8dfd05a5785d1ea4ca9bd920f21db9711d
Sébastien De Pauw <sebastien.depauw@student.hogent.be>
2020-12-09 23:40:46 +0100

    Added phases

--- P3Backend/P3Backend/Model/RoadMapItem.cs
+++ P3Backend/P3Backend/Model/RoadMapItem.cs
@@ -3,10 +3,11 @@ using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace P3Backend.Model {
-    public class RoadMapItem {
-        private string _title;
-        private DateTime _startDate;
-        private DateTime _endDate;
+	public class RoadMapItem {
+		private string _title;
+		private DateTime _startDate;
+		private DateTime _endDate;
+		private RoadmapItemPhase _phase;
 
         public int Id { get; set; }
         [Required]
@@ -37,32 +38,51 @@ namespace P3Backend.Model {
             }
         }
 
-        [Required]
-        public DateTime EndDate {
-            get { return _endDate; }
-            set {
-                if (value <= StartDate)
-                    throw new ArgumentException("End must be after start");
-                else
-                    _endDate = value;
-            }
-        }
+		[Required]
+		public DateTime EndDate {
+			get { return _endDate; }
+			set {
+				if (value <= StartDate)
+					throw new ArgumentException("End must be after start");
+				else
+					_endDate = value;
+			}
+		}
+		
+		public RoadmapItemPhase Phase {
+			get {
+				return _phase;
+			}
+			set
+			{
+				if (value != null)
+				{
+					_phase = value;
+				}
+				else
+				{
+					_phase = RoadmapItemPhase.Preparationphase;
+				}
+			}
+		}
 
-        public RoadMapItem(string title, DateTime start, DateTime end) {
-            Title = title;
-            StartDate = start;
-            EndDate = end;
-            _done = false;
-        }
+		public RoadMapItem(string title, DateTime start, DateTime end) {
+			Title = title;
+			StartDate = start;
+			EndDate = end;
+			_done = false;
+			Phase = RoadmapItemPhase.Preparationphase;
+		}
 
         protected RoadMapItem() {
             // EF
         }
 
-        public void Update(RoadMapItemDTO dto) {
-            Title = dto.Title;
-            _startDate = dto.StartDate;
-            EndDate = dto.EndDate;
-        }
-    }
+		public void Update(RoadMapItemDTO dto) {
+			Title = dto.Title;
+			_startDate = dto.StartDate;
+			EndDate = dto.EndDate;
+			Phase = dto.Phase;
+		}
+	}
 }
